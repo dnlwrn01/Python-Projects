27531bbe21c671ef007fe9fdbd6822bfb8883399
dnlwrn01 <78996771+dnlwrn01@users.noreply.github.com>
2021-04-19 14:19:06 -0500

    Revised polymorphism project

--- polymorphism.py
+++ polymorphism.py
@@ -2,26 +2,33 @@
 
 #parent class
 class User:
-    name = "Daniel Warren"
+    name = "Tom"
     email = "test@email.com"
     password = "helloworld"
     accessLevel = 1
 
+    #login fuction
     def getLoginInfo(self):
             print("Login Below\n-------------")
             entry_email = input("Enter Email: \n>>> ")
             entry_pass = input("Enter Password: \n>>> ")
-
+            
+            #check equality
             if(entry_email == self.email and entry_pass == self.password and self.accessLevel == 1):
                 print(f"Welcome back, {self.name}!")
-            elif (accessLevel != 1):
+
+            #check access level
+            elif (self.accessLevel != 1):
                 print("You do not have clearance to view this information. Contact Admin.")
+
+            #print if notmatch
             else:
                 print("The password or email entered are incorrect.")
 
 #child class
 class Manager(User):
-    pinNum: 1111
+    name = "Jake"
+    pinNum = '1111'
     accessLevel = 2
 
     def getLoginInfo(self):
@@ -31,14 +38,15 @@ class Manager(User):
 
             if(entry_email == self.email and entry_pin == self.pinNum and self.accessLevel == 2):
                 print(f"Welcome back, {self.name}!")
-            elif (accessLevel != 2):
+            elif (self.accessLevel != 2):
                 print("You do not have clearance to view this information. Contact Admin.")
             else:
                 print("The password or email entered are incorrect.")
 
 #child class
 class Admin(User):
-    pinNum: 0000
+    name = "Daniel"
+    pinNum = '0000'
     accessLevel = 0
 
     def getLoginInfo(self):
@@ -48,14 +56,19 @@ class Admin(User):
 
             if(entry_email == self.email and entry_pin == self.pinNum and self.accessLevel == 0):
                 print(f"Welcome back, {self.name}!")
-            elif (accessLevel != 0):
+            elif (self.accessLevel != 0):
                 print("You do not have clearance to view this information. Contact Admin.")
             else:
-                print("The password or email entered are incorrect,")
+                print("The password or email entered are incorrect.")
 
 
 if __name__ == '__main__':
-
+    u = User()
+    u.getLoginInfo()
+    
+    m = Manager()
+    m.getLoginInfo()
+   
     a = Admin()
     a.getLoginInfo()
 
